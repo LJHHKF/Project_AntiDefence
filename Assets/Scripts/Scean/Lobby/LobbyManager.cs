7543d2071080d5d46302a7f23bd05796d25cb62c
LJHHKF <43880473+LJHHKF@users.noreply.github.com>
2020-10-06 20:16:56 +0900

    복구 merge 도전 (#12)
    * Attack_Range 오브젝트의 메쉬&렌더러 삭제
    
    예전에는 색 진해지는 것으로 오브젝트 활성 표시했었으나, 현재는 아예 투명 오브젝트여서 메쉬&렌더러가 필요없다고 판단하여 콜라이더만 남기고 나머지는 삭제.
    
    * 깃헙 사용 Test용
    
    빈 오브젝트 하나를 만들어서 프리팹으로 바꿔본 결과입니다.
    
    * Test 했던것 취소
    
    * Revert "Attack_Range 오브젝트의 메쉬&렌더러 삭제"
    
    This reverts commit 5478ec90875d6bb6bb77b38b45b4241cd7873902.
    
    * 이펙트들 적용
    
    충격파 타워 이펙트를 완성품으로 변경
    노멀 타워 이펙트는 자동 변경됨
    스타트 이펙트 새로 적용 완료
    배리어 이펙트 새로 적용 완료
    
    * 벽, 타일 편의 세팅
    
    원경의 벽 추가
    스테이지 매니저서 인덱스 설정해주면, 프리팹에 배열로 등록된 이미지들(스프라이트, 마테리얼)들서 해당 인덱스의 요소를 뽑아 적용하는 편의 기능 추가.
    
    * Update from Main 을 통해 정상적으로 작업물과 합침.
    
    다음부터는 작업하기 전에 [Branch] - [Update from Main] 또는
    [Branch] - [Merge into current Branch] - [Main] 을 선택하여 변경점을 제대로 적용 후 작업 착수할 것.
    
    * Tile&Wall Setting 방식 수정
    
    기존은 프리팹서 이미지들을 배열로 가지고,
    스테이지 매니저서 인덱스를 관리하는 방식이었으나, 스테이지 매니저에서 둘 모두 가지고서, 각 타일과 월은 게임 시작 시 스테이지 매니저의 정보를 읽은 후 이미지를 가져와서 자신에게 적용하는 형식으로 함.
    
    * Revert "Tile&Wall Setting 방식 수정"
    
    This reverts commit 87c62cfd42b7a22aef132b3a6aba5ead10516ded.
    
    * Tile&Wall Setting 수정 (재작성)
    
    기존은 프리팹서 이미지들을 배열로 가지고,
    스테이지 매니저서 인덱스를 관리하는 방식이었으나, 스테이지 매니저에서 둘 모두 가지고서, 각 타일과 월은 게임 시작 시 스테이지 매니저의 정보를 읽은 후 이미지를 가져와서 자신에게 적용하는 형식으로 함.
    
    (재작성 사유)
    Pull Request 이전에 Update from Main 을 했더니, 금방 작업한 내용을 지우고서 현재 Main 상태의 작업 상태로 돌려버린 탓.
    Main 상태로 덮어 씌우곤 시작하겠다, 면 Update from
    Main과 내 브랜치의 차이점을 찾아서 그만큼만 추가하겠다, 하면 Merge into ~ 인 듯 함.
    
    * Effect 수정하여 재적용
    
    일부 이펙트 미적용 및,
    잘못된 오브젝트와 연결했던 점 등이 있었어서 수정.
    
    * 캐릭터 애니메이션 적용 완료
    
    게임 매니저의 스킨 매니저에서 스킨과 같은 인덱스로 애니메이터를 관리하며,
    현재 나온 아이들과 공격 모션을 타워 공격을 조건부로 하여 1초간 변경되도록 구현 완료. (애니메이션 길이도 1초라 실질 1회만 반복)
    
    * 스킨, 애니메이션 시스템 완성
    
    직전에 커밋한 버전에선 잘못 구동되고 있었음.
    RuntimeAnimatorController 방식을 통해 제대로 구동 중에 애니메이터 변경하며 적용함.
    로비화면과 스킨서도 구동 되도록 추가함.
    
    * 오류남
    
    * ver0.111 수동 복구
    
    * 오류 수정 완. 복구

--- Assets/Scripts/Scean/Lobby/LobbyManager.cs
+++ Assets/Scripts/Scean/Lobby/LobbyManager.cs
@@ -17,6 +17,7 @@ public class LobbyManager : MonoBehaviour
     private Text txt_money;
     private Text txt_level;
     private Image img_skin;
+    private Animator m_animator;
 
     void Start()
     {
@@ -30,6 +31,9 @@ public class LobbyManager : MonoBehaviour
         txt_money = t_subButtons.Find("Panel_Money").Find("Text").GetComponent<Text>();
         txt_level = t_subButtons.Find("Panel_Level").Find("Text").GetComponent<Text>();
         img_skin = gameObject.transform.Find("Panel_Main").Find("Image_Character").GetComponent<Image>();
+        m_animator = gameObject.transform.Find("Panel_Main").Find("Image_Character").GetComponent<Animator>();
+
+        m_animator.runtimeAnimatorController = skinM.anims[skinM.GetSkinIndex()];
 
         txt_money.text = itemM.own_money.ToString();
         txt_level.text = "아직 미구현";
