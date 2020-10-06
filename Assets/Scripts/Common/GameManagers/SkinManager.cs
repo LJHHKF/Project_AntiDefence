859e3e1951c79030f44503e0dbfadf5af1d2f8e7
LJHHKF <43880473+LJHHKF@users.noreply.github.com>
2020-10-06 20:15:25 +0900

    오류 수정 완. 복구

--- Assets/Scripts/Common/GameManagers/SkinManager.cs
+++ Assets/Scripts/Common/GameManagers/SkinManager.cs
@@ -1,17 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class SkinManager : MonoBehaviour
 {
     public Sprite[] skins;
-<<<<<<< HEAD
-
     public RuntimeAnimatorController[] anims;
-=======
-    public AnimatorController[] anims;
->>>>>>> parent of 61564a4... 스킨, 애니메이션 시스템 완성
-
 
     private int skin_index;
 
