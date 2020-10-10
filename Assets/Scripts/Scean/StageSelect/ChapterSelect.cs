76a5bfe3477e119a1fdadd538fc7c69099bbcce4
LJHHKF <43880473+LJHHKF@users.noreply.github.com>
2020-10-11 01:21:19 +0900

    SFX 적용(3) & 상점씬 스크립트 일부 정리
    (SFX 적용 (3))
    기존 스테이지 내부 버튼음으로 착각한, 모든 씬의 버튼 효과음을 모든 씬에서 구동하도록 기능 추가 및 적용 완료.
    
    (상점씬 스크립트 일부 정리)
    일부 반복되던 코드를 한 함수로 정리해서 코드 길이수를 줄임.

--- Assets/Scripts/Scean/StageSelect/ChapterSelect.cs
+++ Assets/Scripts/Scean/StageSelect/ChapterSelect.cs
@@ -8,23 +8,26 @@ public class ChapterSelect : MonoBehaviour
     private GameObject gm;
     private LoadingManager loadingM;
     private BGM_Manager bgmM;
-
+    private AudioManager audioM;
     void Start()
     {
         gm = GameObject.FindGameObjectWithTag("GameManager");
         loadingM = gm.GetComponent<LoadingManager>();
         bgmM = gm.GetComponent<BGM_Manager>();
+        audioM = gm.GetComponent<AudioManager>();
         
         bgmM.Play_LobbyAndShop();
     }
 
     public void BTN_Chp0()
     {
+        audioM.SFX_BTN_Click();
         loadingM.LoadScene("Chp00_StageSelect");
     }
 
     public void BTN_Return()
     {
+        audioM.SFX_BTN_Click();
         loadingM.LoadScene("Lobby");
     }
 }
