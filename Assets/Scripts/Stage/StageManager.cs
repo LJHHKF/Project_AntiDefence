fe2b3f5dcbf591b64f4e25cd4ae7f4e7fea885ff
LJHHKF <43880473+LJHHKF@users.noreply.github.com>
2020-10-22 01:11:50 +0900

    10.22(1) 오브젝트 풀링 적용
    (10.22 (1))
    ‘오브젝트 풀링’ 방식을 적용할 수 있는 부분들엔 적용하여 최적화 작업 진행
    상세는 일지의 22일자 부분 체크.

--- Assets/Scripts/Stage/StageManager.cs
+++ Assets/Scripts/Stage/StageManager.cs
@@ -11,7 +11,7 @@ public class StageManager : MonoBehaviour
     public Transform p_enemySpawnPoints;
     public GameObject b_spawnPoints;
     private Transform[] c_enemySpawnPoints;
-    public GameObject spawn_Effects;
+    public GameObject spawn_Effect;
     public GameObject e_direction_Effects;
     public GameObject[] enemies;
     private GameObject ui_Canvas;
@@ -23,6 +23,18 @@ public class StageManager : MonoBehaviour
     public float[] delayTimesIndex = { 1.0f, 1.0f, 2.0f, 2.0f, 1.0f };
     public int[] spawnPointIndex = { 1, 9, 2, 10, 15 }; // 0은 부모 위치이므로, 캐릭터 바로 옆 생성이므로 주의
     public int[] spawnEnemyIndex = { 0, 0, 0, 0, 0 };
+    private Transform t_objPools;
+    private Transform t_objectPool_Enemy;
+    private List<GameObject> listPool_Enemy = new List<GameObject>();
+    private int cnt_enemyPool = 0;
+    private Transform t_objectPool_SpawnEf;
+    private List<GameObject> listPool_SpawnEf = new List<GameObject>();
+    private int cnt_spEfPool = -1;
+    private bool is_serched_sp = false;
+    private Transform t_objectPool_DirEffect;
+    private List<GameObject> listPool_DirEf = new List<GameObject>();
+    private int cnt_DirEfPool = -1;
+    private bool is_serched_dir = false;
 
     [Header ("Chp,Stage info")]
     public int chpter_num = 0;
@@ -98,6 +110,11 @@ public class StageManager : MonoBehaviour
         itemM = gm.GetComponent<SelectedItemManager>();
         bgmM = gm.GetComponent<BGM_Manager>();
 
+        t_objPools = GameObject.FindGameObjectWithTag("ObjectPools").transform;
+        t_objectPool_Enemy = t_objPools.Find("Enemies");
+        t_objectPool_SpawnEf = t_objPools.Find("SpawnEffect");
+        t_objectPool_DirEffect = t_objPools.Find("DirEffect");
+
 
         towerBoard = GameObject.FindGameObjectWithTag("TowerBoard");
         ta_M = towerBoard.GetComponent<TA_Manager>();
@@ -116,6 +133,8 @@ public class StageManager : MonoBehaviour
         {
             b_spawnPoints.SetActive(false);
         }
+
+        EnemyPooling();
 
         GameObject effect = Instantiate(startEffect, gameObject.transform);
         GameObject effect2 = Instantiate(startEffect2, gameObject.transform);
@@ -165,13 +184,102 @@ public class StageManager : MonoBehaviour
         }
     }
 
-    private void SpawnEnemy(int sp_index, int m_index)
+    private void SpawnEnemy(int sp_index)
     {
-        Instantiate(enemies[m_index], c_enemySpawnPoints[sp_index].position, Quaternion.identity) ;
-        GameObject effect = Instantiate(spawn_Effects, c_enemySpawnPoints[sp_index]);
-        Destroy(effect, 2.0f);
-        GameObject d_effect = Instantiate(e_direction_Effects, c_enemySpawnPoints[sp_index]);
-        Destroy(d_effect, 1.0f);
+        GameObject enemy = listPool_Enemy[cnt_enemyPool++];
+        enemy.transform.position = c_enemySpawnPoints[sp_index].position;
+        enemy.transform.rotation = Quaternion.identity;
+        enemy.SetActive(true);
+
+        PullingSpawnEffect(sp_index, 2.0f);
+        PullingDirEffect(sp_index, 1.0f);
+    }
+
+    private void EnemyPooling()
+    {
+        for (int i = 0; i < spawnEnemyIndex.Length; i++)
+        {
+            var enemy = Instantiate(enemies[spawnEnemyIndex[i]], t_objectPool_Enemy);
+            enemy.name = "Enemy_" + i.ToString("000");
+            enemy.SetActive(false);
+            listPool_Enemy.Add(enemy);
+        }
+    }
+
+    private void PullingSpawnEffect(int sp_index, float time)
+    {
+        if(cnt_spEfPool < 0)
+        {
+            SpawnEffectPooling();
+        }
+
+        is_serched_sp = false;
+        for (int i = 0; i < cnt_spEfPool; i++)
+        {
+            if (listPool_SpawnEf[i].activeSelf == false)
+            {
+                listPool_SpawnEf[i].transform.position = c_enemySpawnPoints[sp_index].position;
+                listPool_SpawnEf[i].transform.rotation = c_enemySpawnPoints[sp_index].rotation;
+                listPool_SpawnEf[i].SetActive(true);
+                StartCoroutine(StopEffect(listPool_SpawnEf[i], time));
+                is_serched_sp = true;
+            }
+        }
+        if(is_serched_sp == false)
+        {
+            SpawnEffectPooling();
+            PullingSpawnEffect(sp_index, time);
+        }
+    }
+
+    private void PullingDirEffect(int sp_index, float time)
+    {
+        if (cnt_DirEfPool < 0)
+        {
+            DirEffectPooling();
+        }
+        is_serched_dir = false;
+        for (int i = 0; i < cnt_DirEfPool; i++)
+        {
+            if(listPool_DirEf[i].activeSelf == false)
+            {
+                listPool_DirEf[i].transform.position = c_enemySpawnPoints[sp_index].position;
+                listPool_DirEf[i].transform.rotation = c_enemySpawnPoints[sp_index].rotation;
+                listPool_DirEf[i].SetActive(true);
+                StartCoroutine(StopEffect(listPool_DirEf[i], time));
+                is_serched_dir = true;
+            }
+        }
+        if (is_serched_dir == false)
+        {
+            DirEffectPooling();
+            PullingDirEffect(sp_index, time);
+        }
+    }
+
+    private void SpawnEffectPooling()
+    {
+        cnt_spEfPool++;
+        var effect = Instantiate(spawn_Effect, t_objectPool_SpawnEf);
+        effect.name = "sp_Effect_" + cnt_spEfPool.ToString("000");
+        effect.SetActive(false);
+        listPool_SpawnEf.Add(effect);
+    }
+
+    private void DirEffectPooling()
+    {
+        cnt_DirEfPool++;
+        var effct = Instantiate(e_direction_Effects, t_objectPool_DirEffect);
+        effct.name = "dir_Effect_" + cnt_DirEfPool.ToString("000");
+        effct.SetActive(false);
+        listPool_DirEf.Add(effct);
+    }
+
+    IEnumerator StopEffect(GameObject effect, float time)
+    {
+        yield return new WaitForSeconds(time);
+        effect.SetActive(false);
+        yield break;
     }
 
     IEnumerator CountTimeForSpawn()
@@ -184,7 +292,7 @@ public class StageManager : MonoBehaviour
         {
             if (spawnPointIndex.Length > count)
             {
-                SpawnEnemy(spawnPointIndex[count], spawnEnemyIndex[count]);
+                SpawnEnemy(spawnPointIndex[count]);
                 count++;
             }
             else
